36360713f9db53be23572b12968626f3e36485c2
mzuk <mzuk@pgs-soft.com>
2021-05-19 20:48:53 +0200

    get data from POST request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
+import * as bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -12,6 +13,10 @@ declare namespace Express {
 const PORT = 3000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 type Tag = {
   title: string;
@@ -53,6 +58,17 @@ app.get("/tags/:tag", (req, res) => {
   return res
     .status(200)
     .json({ data: tags.find((element) => element.title.toLowerCase() === tag) });
+});
+
+
+//POST
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/name", function(req, res) {
+  const { body: {first, last} } = req;
+  const name = `${first} ${last}`
+  res.json({ name });
 });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
